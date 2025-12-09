c6ca972bdb88ab081a5ac06741c2635def365c9b
Yago Mfh <yagomfh@gmail.com>
2025-12-09 14:12:36 +0100

    feat: revert back to node

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,16 @@
 import { config } from "dotenv";
-
-import { drizzle } from 'drizzle-orm/bun-sql';
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema.ts";
 import * as authSchema from "./auth-schema.ts";
 
 config();
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema: { ...schema, ...authSchema } });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const db = drizzle(pool, {
+  schema: { ...schema, ...authSchema },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,37 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = defineConfig({
-  optimizeDeps: {
-    exclude: ["bun"]
-  },
-  ssr: {
-    external: ["bun"]
-  },
   resolve: {
     alias: {
-      'lucide-react/dynamic': path.resolve(__dirname, 'node_modules/lucide-react/dynamic.mjs'),
+      "lucide-react/dynamic": path.resolve(
+        __dirname,
+        "node_modules/lucide-react/dynamic.mjs"
+      ),
     },
   },
   plugins: [
     devtools(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
     tanstackStart(),
     nitro({
-      vercel: {
-        functions: {
-          runtime: "bun1.x",
-        }
-      },
+      preset: "vercel",
     }),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
