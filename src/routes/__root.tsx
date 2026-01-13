14d34fc478436b5dfa0f81ffa4b1aa7918e7331d
Yago Mfh <yagomfh@gmail.com>
2026-01-13 11:48:55 +0100

    feat: add analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import {
-	createRootRouteWithContext,
-	HeadContent,
-	Scripts,
+  createRootRouteWithContext,
+  HeadContent,
+  Scripts,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import type { TRPCOptionsProxy } from "@trpc/tanstack-react-query";
@@ -13,83 +13,85 @@ import { Toaster } from "@/components/ui/sonner";
 import type { TRPCRouter } from "@/integrations/trpc/router";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import appCss from "../styles.css?url";
+import { Analytics } from "@vercel/analytics/react";
 
 interface MyRouterContext {
-	queryClient: QueryClient;
+  queryClient: QueryClient;
 
-	trpc: TRPCOptionsProxy<TRPCRouter>;
+  trpc: TRPCOptionsProxy<TRPCRouter>;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-	head: () => ({
-		meta: [
-			{
-				charSet: "utf-8",
-			},
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
-				title: "Home | delivrr.work",
-			},
-			{
-				name: "description",
-				content: "delivrr.work is a platform for automated summaries of your github pushes.",
-			},
-		],
-		links: [
-			{
-				rel: "stylesheet",
-				href: appCss,
-			},
-			{
-				rel: "icon",
-				href: "/favicon.ico",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				href: "/favicon-96x96.png",
-				sizes: "96x96",
-			},
-			{
-				rel: "icon",
-				type: "image/svg+xml",
-				href: "/favicon.svg",
-			},
-			{
-				rel: "shortcut icon",
-				href: "/favicon.ico",
-			},
-			{
-				rel: "apple-touch-icon",
-				sizes: "180x180",
-				href: "/apple-touch-icon.png",
-			},
-			{
-				name: "apple-mobile-web-app-title",
-				content: "delivrr.work",
-			},
-			{
-				rel: "manifest",
-				href: "/site.webmanifest",
-			},
-		],
-	}),
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Home | delivrr.work",
+      },
+      {
+        name: "description",
+        content:
+          "delivrr.work is a platform for automated summaries of your github pushes.",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        name: "apple-mobile-web-app-title",
+        content: "delivrr.work",
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest",
+      },
+    ],
+  }),
 
-	shellComponent: RootDocument,
+  shellComponent: RootDocument,
 });
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head>
-				<HeadContent />
-				{/* biome-ignore lint: Required to prevent theme flash */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <HeadContent />
+        {/* biome-ignore lint: Required to prevent theme flash */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
 							(function() {
 								const storageKey = 'vite-ui-theme';
 								const theme = localStorage.getItem(storageKey) || 'system';
@@ -103,28 +105,29 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 								}
 							})();
 						`,
-					}}
-				/>
-			</head>
-			<body>
-				<ThemeProvider>
-					{children}
-					<Toaster />
-					<TanStackDevtools
-						config={{
-							position: "bottom-right",
-						}}
-						plugins={[
-							{
-								name: "Tanstack Router",
-								render: <TanStackRouterDevtoolsPanel />,
-							},
-							TanStackQueryDevtools,
-						]}
-					/>
-				</ThemeProvider>
-				<Scripts />
-			</body>
-		</html>
-	);
+          }}
+        />
+      </head>
+      <body>
+        <ThemeProvider>
+          {children}
+          <Toaster />
+          <TanStackDevtools
+            config={{
+              position: "bottom-right",
+            }}
+            plugins={[
+              {
+                name: "Tanstack Router",
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+              TanStackQueryDevtools,
+            ]}
+          />
+        </ThemeProvider>
+        <Analytics />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
